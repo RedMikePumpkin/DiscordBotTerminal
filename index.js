f4acae6656b4a581ac4657098a965e98736b14a2
RedMikePumpkin <michael@malinov.com>
2020-03-01 23:10:38 -0800

    yes

---
+++ index.js
@@ -0,0 +1,30 @@
+var Discord = require('discord.js');
+
+async function main() {
+  var client = new Discord.Client();
+  process.stdout.write("enter bot token: ");
+  var token = await readline();
+  client.on("ready", () => {
+    console.log("Logged in");
+  });
+  client.on("message", msg => {
+    console.log(msg.author.username + msg.author.discriminator + ": " + msg.content)
+  })
+  client.login(token);
+}
+
+
+main();
+var readline__callback;
+function readline(cb) {
+  if (cb !== undefined) readline__callback = cb;
+  else return new Promise(yey => {
+    readline__callback = yey;
+  });
+  return null;
+}
+var stdin = process.openStdin();
+stdin.addListener("data", function (d) {
+  readline__callback(d.toString().trim());
+});
+stdin.addListener("end", function () {});
