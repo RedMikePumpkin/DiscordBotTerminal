0d6be03bd64b6b8ecb138fa608662e4e7361fcdd
RedMikePumpkin <michael@malinov.com>
2020-03-02 19:39:56 -0800

    yes

--- index.js
+++ index.js
@@ -1,27 +1,54 @@
 var Discord = require('discord.js');
+var fs = require('fs');
 
 async function main() {
-  var client = new Discord.Client();
-  process.stdout.write("enter bot token: ");
-  var token = await readline();
-  client.on("ready", () => {
-    console.log("Logged in");
-  });
-  client.on("message", msg => {
-    console.log(msg.author.username + "#" + msg.author.discriminator + ": " + msg.content)
-  })
-  client.login(token);
+  var op = process.argv.slice(2)[0].toLowerCase();
+  console.log("started");
+  if (op === "start") {
+    var client = new Discord.Client();
+    var configs = await loadConfigs();
+    client.on("ready", () => {
+      console.log("Logged in");
+    });
+    client.on("message", msg => {
+      console.log("<" + msg.channel.name + "> " +  msg.author.username + "#" + msg.author.discriminator + ": " + msg.content);
+    });
+    client.login(configs.token);
+  } else if (op === "init") {
+    await new Promise(yey => {
+      fs.access(file, fs.constants.F_OK, (err) => {
+        if (!err) {
+          process.stdout.write("the config file already exists, do you want to override it with the default value? (y/n) ");
+          if (await readline() === "y") {
+            fs.copyFile("./config_example.json", "./config.json", () => {
+              console.log("copy done!");
+              yey();
+            });
+          } else {
+            console.log("aborting")
+            yey();
+          }
+        } else {
+          fs.copyFile("./config_example.json", "./config.json", () => {
+            console.log("copy done!");
+            yey();
+          });
+        } 
+      });
+    });
+    process.exit(0);
+  }
 }
 
 
 main();
 var readline__callback;
 function readline(cb) {
-  if (cb !== undefined) readline__callback = cb;
-  else return new Promise(yey => {
+  if (cb === undefined) return new Promise(yey => {
     readline__callback = yey;
   });
-  return null;
+  readline__callback = cb;
+  return new Promise(yey => yey());
 }
 var stdin = process.openStdin();
 stdin.addListener("data", function (d) {
@@ -29,3 +56,7 @@ stdin.addListener("data", function (d) {
   readline__callback = undefined;
 });
 stdin.addListener("end", function () {});
+
+function loadConfigs() {
+  
+}
