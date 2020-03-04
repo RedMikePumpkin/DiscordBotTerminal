8aed010b562d4f42ea247fbf594f59248e495e39
RedMikePumpkin <michael@malinov.com>
2020-03-04 07:47:25 -0800

    a

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
 var Discord = require('discord.js');
 var fs = require('fs');
+var configs;
+var stream = fs.createWriteStream("logs.txt", {flags:'a'});
 
 async function main() {
   var op = process.argv.slice(2)[0].toLowerCase();
-  console.log("started");
+  console.log("started, doing " + op);
   if (op === "start") {
     var client = new Discord.Client();
-    var configs = await loadConfigs();
-    client.on("ready", () => {
+    await loadConfigs();
+    var config = configs[0];
+    var channels = client.channels.cache.array();
+    console.log(channels)
+    client.on("ready", async function() {
       console.log("Logged in");
+      while(1) {
+        var channel = await readline();
+        var message = await readline();
+        channels.find(i => i.client.name === channel).send(message)
+          .then(message => console.log(`Sent: ${message.content}`))
+          .catch(console.error);
+      }
     });
     client.on("message", msg => {
-      console.log("<" + msg.channel.name + "> " +  msg.author.username + "#" + msg.author.discriminator + ": " + msg.content);
+      console.log("<#" + msg.channel.name + "> " +  msg.author.username + "#" + msg.author.discriminator + ": " + msg.content);
+      msg._authun = msg.author.username;
+      msg._channel = msg.channel.name;
+      msg._disc = msg.author.discriminator;
+      stream.write(JSON.stringify(msg) + "\n");
     });
-    client.login(configs.token);
+    client.login(config.token);
   } else if (op === "init") {
     await new Promise(yey => {
       fs.access(file, fs.constants.F_OK, (err) => {
         if (!err) {
           process.stdout.write("the config file already exists, do you want to override it with the default value? (y/n) ");
-          if (await readline() === "y") {
+          if ((await (readline())) === "y") {
             fs.copyFile("./config_example.json", "./config.json", () => {
               console.log("copy done!");
               yey();
@@ -58,5 +74,15 @@ stdin.addListener("data", function (d) {
 stdin.addListener("end", function () {});
 
 function loadConfigs() {
-  
+  return new Promise(yey => {
+    fs.readFile("./config.json", (err, data) => {
+      if (err) {
+        console.log("error reading config file, aborting");
+        process.exit(0);
+      } else {
+        configs = JSON.parse(data.toString()).servers;
+        yey();
+      }
+    })
+  })
 }
